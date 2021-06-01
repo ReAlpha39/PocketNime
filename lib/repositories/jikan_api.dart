b45eb50d03223a1f1d845bf764b2bc7583ffa22b
ReAlpha39 <yunusw718@gmail.com>
2021-06-01 23:38:43 +0800

    jikan_api: add function to fetch season anime

--- lib/repositories/jikan_api.dart
+++ lib/repositories/jikan_api.dart
@@ -1,4 +1,5 @@
 import 'package:anipocket/models/constant.dart';
+import 'package:anipocket/models/season/season_anime.dart';
 import 'package:anipocket/models/tops.dart';
 import 'package:anipocket/models/type.dart';
 import 'package:http/http.dart' as http;
@@ -6,11 +7,11 @@ import 'package:http/http.dart' as http;
 class JikanApi {
   // Fetch top data by type
   Future<Tops> getTop(TopType type, {int? page, TopSubType? subType}) async {
-    var path = version + '/top/${typeValues.reverse[type]}';
+    var path = version + '/top/${topTypeValues.reverse[type]}';
     if (page != null) {
       path += '/$page';
     } else if (subType != null) {
-      path += '/${typeValues.reverse[subType]}';
+      path += '/${topTypeValues.reverse[subType]}';
     }
     var output;
     try {
@@ -20,6 +21,25 @@ class JikanApi {
       }
     } catch (e) {
       print(e);
+    }
+    return output;
+  }
+
+  // Fetch this season anime
+  Future<SeasonAnime> getSeasonAnime(
+      {int? year, SeasonType? seasonType}) async {
+    var path = version + '/season';
+    if (year != null && seasonType != null) {
+      path += '/$year/${seasonTypeValues.reverse[seasonType]!.toLowerCase()}';
+    }
+    SeasonAnime output = SeasonAnime();
+    try {
+      var response = await http.get(Uri.http(baseUrl, path));
+      if (response.statusCode == 200) {
+        output = SeasonAnime.fromJson(response.body);
+      }
+    } catch (e) {
+      throw "Cannot fetch season anime";
     }
     return output;
   }
