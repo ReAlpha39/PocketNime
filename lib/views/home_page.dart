fc416a78b14c1daa5d841f6b7c0dd3dc44f9f14e
ReAlpha39 <yunusw718@gmail.com>
2021-06-02 16:07:56 +0800

    home_page: change count grid based screen width

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:anipocket/controllers/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
@@ -19,15 +20,32 @@ class HomePage extends StatelessWidget {
                   child: CircularProgressIndicator(),
                 )
               : Scrollbar(
-                  child: ListView.builder(
-                    itemCount:
-                        _homeController.seasonAnime.value.animeList!.length,
-                    itemBuilder: (context, index) =>
-                        _homeController.loadSeasonAnimeWidget(index),
+                  child: LayoutBuilder(
+                    builder: (context, constraint) {
+                      if (constraint.maxWidth <= 600) {
+                        return _cardGridView(1);
+                      } else if (constraint.maxWidth <= 1200) {
+                        return _cardGridView(2);
+                      } else {
+                        return _cardGridView(3);
+                      }
+                    },
                   ),
                 ),
         ),
       ),
+    );
+  }
+
+  Widget _cardGridView(int crossAxisCount) {
+    return StaggeredGridView.countBuilder(
+      itemCount: _homeController.seasonAnime.value.animeList!.length,
+      itemBuilder: (context, index) =>
+          _homeController.loadSeasonAnimeWidget(index),
+      crossAxisCount: crossAxisCount,
+      staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
     );
   }
 }
