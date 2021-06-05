68b6753655137a05922fe24b85b4bd248b767c84
ReAlpha39 <yunusw718@gmail.com>
2021-06-05 22:11:12 +0800

    anime_main_card: change ui

--- lib/widgets/anime_detail/anime_main_card.dart
+++ lib/widgets/anime_detail/anime_main_card.dart
@@ -10,54 +10,97 @@ class AnimeMainCard extends StatelessWidget {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(8),
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Obx(
-            () => ConstrainedBox(
-              constraints: BoxConstraints(maxHeight: 220),
-              child: BoxImage(
-                pathPicture: _controller.anime.value.imageUrl,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxHeight: 220),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Obx(
+              () => ConstrainedBox(
+                constraints: BoxConstraints(maxHeight: 220),
+                child: BoxImage(
+                  pathPicture: _controller.anime.value.imageUrl,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                _controller.anime.value.titleJapanese == null
-                    ? Container()
-                    : Container(
-                        padding: const EdgeInsets.all(8.0),
-                        color: Colors.grey.shade200,
-                        width: double.infinity,
-                        child: Text(
-                          _controller.anime.value.titleJapanese!,
-                          overflow: TextOverflow.clip,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _controller.anime.value.title == null
+                      ? Container()
+                      : Container(
+                          padding: const EdgeInsets.all(8.0),
+                          color: Colors.grey.shade100,
+                          width: double.infinity,
+                          child: Text(
+                            _controller.anime.value.title!,
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
                           ),
                         ),
-                      ),
-                _controller.anime.value.titleEnglish == null
-                    ? Container()
-                    : Container(
-                        padding: const EdgeInsets.all(8.0),
-                        color: Colors.grey.shade100,
-                        child: Text(
-                          _controller.anime.value.titleEnglish!,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
+                  _controller.anime.value.titleJapanese == null
+                      ? Container()
+                      : Container(
+                          padding: const EdgeInsets.all(8.0),
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Japanese",
+                                overflow: TextOverflow.clip,
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Text(
+                                _controller.anime.value.titleJapanese!,
+                                overflow: TextOverflow.clip,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
                           ),
-                          overflow: TextOverflow.clip,
                         ),
-                      ),
-              ],
+                  _controller.anime.value.titleEnglish == null
+                      ? Container()
+                      : Container(
+                          padding: const EdgeInsets.all(8.0),
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "English",
+                                overflow: TextOverflow.clip,
+                                style: TextStyle(
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Text(
+                                _controller.anime.value.titleEnglish!,
+                                overflow: TextOverflow.clip,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
